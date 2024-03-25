b3eb062572ae382d27f1d783298f83696add3587
aarushrastogi123 <aarushrastogi123@users.noreply.github.com>
2024-03-25 18:31:00 +0530

    python codes

--- caeser cipher.py
+++ caeser cipher.py
@@ -1,9 +1,3 @@
-alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction=input("Type 'encode' to encrypt and 'decode' to decrypt: ")
-text=input("\nType your messages here: ").lower()
-shift=int(input("\nType the shift number: "))
 logo = """           
  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
 a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
@@ -22,6 +16,13 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
               88           
 """
 print(logo)
+alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction=input("Type 'encode' to encrypt and 'decode' to decrypt: ")
+text=input("\nType your messages here: ").lower()
+shift=int(input("\nType the shift number: "))
+
 
 def caeser(d,t,s):
     end=''
