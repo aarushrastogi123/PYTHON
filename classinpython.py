2c13dae9f3c48b4657f869bdf5249caa89760f80
aarushrastogi123 <aarushrastogi123@users.noreply.github.com>
2024-07-02 16:45:59 +0530

    python codes

---
+++ classinpython.py
@@ -0,0 +1,12 @@
+'''class User:
+    def __init__(self,user_id,username):
+        self.id=user_id
+        self.name=username
+#user_1=User("001","ABCDEFGH")    
+#print(user_1.id,user_1.name) '''
+
+class Car:
+    def car_race_mode(self):
+        self.seats=2
+user_1=Car()   
+print(user_1.car_race_mode())
